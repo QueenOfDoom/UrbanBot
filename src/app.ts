6e7ce3b65a175dc5752dee238eb0f166978d32f7
Krista Progri <kristaprogri@gmail.com>
2021-04-11 21:41:44 -0400

    feat: Urban Dictionary Look-up
    Added a feature that will search up the user's message on Urban
    Dictionary and return the definition and an example in a nice looking
    Embed

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,28 @@ import { Message } from 'discord.js';
 
 dotenv.config();
 const client = new Discord.Client();
+const urban = require('relevant-urban');
 
-client.on('message', (message: Message) => {
+client.on('message', async (message: Message) => {
+
     if (message.author != client.user) {
-        message.reply(`Received ${message.content}`);
+
+            let result = await urban(message.content).catch(e => {
+                return message.channel.send('Word not found, please try another word.');
+            })
+
+            const embed = new Discord.MessageEmbed()
+            embed.setColor("RED")
+            embed.setTitle(result.word.charAt(0).toUpperCase() + result.word.slice(1))
+            embed.setDescription(`**Definition:** \n *${result.definition}* \n\n **Example:** \n  *${result.example}*`)
+
+            embed.setFooter('Definition From Urban Dictionary')
+
+            message.channel.send(embed);
+
+            await message.react('✅')
     }
+
 });
 
 client.login(<string> process.env.token);
